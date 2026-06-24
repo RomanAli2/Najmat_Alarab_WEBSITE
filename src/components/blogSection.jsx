52aa5abe5e01771f5a50c347d2103c2e971bd072
RomanAli2 <romanali87866677@gmail.com>
2026-06-24 12:46:19 -1000

    Update blog section text to reflect correct company name

--- src/components/blogSection.jsx
+++ src/components/blogSection.jsx
@@ -11,7 +11,7 @@ function BlogSection(){
           <strong className="text-blue-500"> Najamat Alarab</strong>
     </h1><br/>
     <p className="leading-8">
-        Planning your next trip? It is a lot easier now thanks to Quick VISA. In addition to our document clearing services, we also offer flight booking so that you can get to where you are going quickly. Your travel needs are taken care of from start to finish, whether you are traveling for business, vacation, or relocation. Booking your tickets today will make your journey easy.
+        Planning your next trip? It is a lot easier now thanks to Najmat Alarab. In addition to our document clearing services, we also offer flight booking so that you can get to where you are going quickly. Your travel needs are taken care of from start to finish, whether you are traveling for business, vacation, or relocation. Booking your tickets today will make your journey easy.
     </p>
 </div>
 <div className="">
