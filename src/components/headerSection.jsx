04b5b09224634c7d213959d95b4d498b4de609c8
RomanAli2 <romanali87866677@gmail.com>
2026-06-25 18:12:59 -1000

    Update page title for improved branding; enhance HeaderSection styles and spacing

--- src/components/headerSection.jsx
+++ src/components/headerSection.jsx
@@ -40,19 +40,20 @@ function HeaderSection() {
     return(
       
         <header>
-       <div className=" gap-5  top-0 fixed z-50 w-full bg-slate-100 justify-center flex text-md
+       <div className=" gap-5 border-b  top-0 fixed z-50 w-full  bg-slate-100 md:justify-start justify-center flex text-md
          md:text-lg   ">
-        <span className="text-lg md:text-xl">📞 +971 545049455</span>
-        <span className="flex gap-2 text-lg md:text-xl">  
+        <span  className="text-lg md:text-xl m-2">📞 +971 545049455</span>
+        <span className="flex gap-2 text-lg m-2 md:text-xl">  
         <a href="https://www.instagram.com/muhmmadsajid57?igsh=bDl3MzVkZWN2YzEy&utm_source=qr" target="_blank">
-    <i className="fa-brands fa-instagram  text-white rounded-sm bg-linear-to-r from-pink-400 to-red-400"></i> 
+    <i className="fa-brands fa-instagram  text-white 
+    p-0.5 rounded-md bg-linear-to-r from-pink-400 to-red-400"></i> 
   </a>
 
   <a href="https://www.facebook.com/share/17pachg9mb/?mibextid=wwXIfr" target="_blank">
-     <i className="fa-brands fa-facebook text-blue-500"></i> 
+     <i className="fa-brands fa-facebook text-blue-500 text-center  "></i> 
   </a></span>
         </div>
-       <div className=" fixed w-full z-50 top-7  bg-slate-100/90 shadow ">
+       <div className=" fixed w-full z-50 top-9 bg-slate-100/90 shadow ">
         <nav className="flex items-center m-2 justify-between">
         <div className="logo flex gap-1 items-center ">
         {/* Logo */}
@@ -94,7 +95,7 @@ function HeaderSection() {
        </div>
 {toggleHeader &&(
    <div className="flex flex-col text-center fixed w-full
-    bg-white p-2 z-40 top-19 items-center gap-2 md:gap-4">
+    bg-white p-2 z-40 top-21 items-center gap-2 md:gap-4">
         <span className="nav-links w-full flex gap-2 flex-col md:gap-4 items-center">
             <a onClick={()=>setToggleHeader(false)} href="#Home" className=" text- cursor-pointer w-full p-1 hover:bg-gray-200  md:text-lg
         font-serif">Home</a>
